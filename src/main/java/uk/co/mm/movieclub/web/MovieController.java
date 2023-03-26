068ba6c3f37def101375cf0d5f9bb94df1e44713
Michal Mrozek <m.mrozek@hotmail.com>
2023-03-26 21:08:57 +0100

    404 page implemented

--- src/main/java/uk/co/mm/movieclub/web/MovieController.java
+++ src/main/java/uk/co/mm/movieclub/web/MovieController.java
@@ -1,9 +1,11 @@
 package uk.co.mm.movieclub.web;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
 import uk.co.mm.movieclub.domain.movie.MovieService;
 import uk.co.mm.movieclub.domain.movie.dto.MovieDto;
 
@@ -18,8 +20,8 @@ public class MovieController {
     }
     @GetMapping("/film/{id}")
     public String getMovie(@PathVariable long id, Model model){
-    Optional<MovieDto> optionalMovie = movieService.findMovieById(id);
-    optionalMovie.ifPresent(movie -> model.addAttribute("movie", movie));
+    MovieDto movie = movieService.findMovieById(id).orElseThrow(() ->new ResponseStatusException(HttpStatus.NOT_FOUND));
+    model.addAttribute("movie", movie);
     return"movie";
     }
 
