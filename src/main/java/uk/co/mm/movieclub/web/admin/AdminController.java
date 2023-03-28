e3b8cff700bcc8557b883ef3aac6fa81030c462b
Michal Mrozek <m.mrozek@hotmail.com>
2023-03-28 22:45:42 +0100

    new genre form

--- src/main/java/uk/co/mm/movieclub/web/admin/AdminController.java
+++ src/main/java/uk/co/mm/movieclub/web/admin/AdminController.java
@@ -1,10 +1,11 @@
-package uk.co.mm.movieclub.web;
+package uk.co.mm.movieclub.web.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
+    public static final String NOTIFICATION_ATTRIBUTE = "notification";
 
     @GetMapping("/admin")
     public String getAdminPanel() {
